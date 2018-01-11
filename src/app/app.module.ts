40a024ea64269b97a8a6d7b799ea02c51210a87e
krzysiek <krzysiek.milewski@gmail.com>
2018-01-11 14:53:05 +0100

    Ionic App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,52 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import {LoginPage} from "../pages/login/login";
+import {AuthService} from "../providers/auth-service/auth-service";
+import {DataService} from '../providers/data-service/data-service';
+import { UserServiceProvider } from '../providers/user-service/user-service';
+import {Http, HttpModule} from "@angular/http";
+import {RegistrationServiceProvider} from "../providers/registration-service/registration-service";
+import {AuthConfig, AuthHttp} from "angular2-jwt";
+import {TOKEN_NAME} from "../providers/authentication-service/auth.constant";
+import {AuthenticationService} from "../providers/authentication-service/authentication-service";
 
+export function authHttpServiceFactory(http: Http) {
+  return new AuthHttp(new AuthConfig({
+    headerPrefix: 'Bearer',
+    tokenName: TOKEN_NAME,
+    globalHeaders: [{'Content-Type': 'application/json'}],
+    noJwtError: false,
+    noTokenScheme: true,
+    tokenGetter: (() => localStorage.getItem(TOKEN_NAME))
+  }), http);
+}
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    LoginPage
+
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    LoginPage
   ],
   providers: [
+    AuthService,
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    DataService,
+    AuthenticationService,
+    RegistrationServiceProvider,
+    UserServiceProvider
   ]
 })
 export class AppModule {}
---
+++ src/pages/model/cityData.ts
@@ -0,0 +1,6 @@
+export class CityData {
+  public id: number;
+  public name: string;
+  public temp: string;
+  public time: string;
+}
