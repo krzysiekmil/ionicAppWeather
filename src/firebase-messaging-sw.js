237fdd0119888a6b3e176818e814c57dec7912c6
krzysiek <krzysiek.milewski@gmail.com>
2018-01-25 18:14:58 +0100

    nedStark

---
+++ src/firebase-messaging-sw.js
@@ -0,0 +1,13 @@
+importScripts("https://www.gstatic.com/firebasejs/4.9.0/firebase.js")
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyC_LuJhMAX6U_fYrXXb1utAy9r7TKnAPXU",
+  authDomain: "api-project-515150500055.firebaseapp.com",
+  databaseURL: "https://api-project-515150500055.firebaseio.com",
+  projectId: "api-project-515150500055",
+  storageBucket: "api-project-515150500055.appspot.com",
+  messagingSenderId: "515150500055"
+};
+firebase.initializeApp(config);
+
